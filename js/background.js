2c3df2f45e498aa73c05add54564264e854372dd
jiangjinhao <jiangjinhao@vip.qq.com>
2017-09-06 13:07:21 +0800

    优化了一下,整理了一下结构,下一步任务是能够自己动填写地址

--- js/background.js
+++ js/background.js
@@ -20,4 +20,37 @@ chrome.runtime.onMessage.addListener(
 
     });
 
+chrome.commands.onCommand.addListener(function (command) {
+    console.log(command);
+    if (command.valueOf() == "getUserInfo") {
+        //这个和用户点击 复制商品信息的效果是一样的
+        //需要给 pinduoduo.js 发送消息,来获取用户信息
+        console.log("getUserInfo");
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {type: "background_getUserInfo"}, function (response) {
+                name = response.name;
+                phone = response.phone;
+                address = response.address;
+
+                console.log(name);
+                console.log(phone);
+                console.log(address);
+            });
+        });
+    } else if (command.valueOf() == "setUserInfo") {
+        //这个和点击 粘贴到淘宝是一样的
+        //给 taobao.js发送一个消息,
+
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                type: "background_setUserInfo",
+                name: name,
+                phone: phone,
+                address: address
+            }, function (response) {
+                console.log(response.farewell);
+            });
+        });
+    }
+});
 
--- js/pinduoduo.js
+++ js/pinduoduo.js
@@ -24,6 +24,20 @@ chrome.runtime.onMessage.addListener(
                 console.log(response.farewell);
             });
             sendResponse({farewell: "successful"});
+        }else if(request.type=="background_getUserInfo"){
+            //这里是使用快捷键的时候,直接收到的background.js的请求时的操作
+            //1.读取相关信息
+            var userInfo = document.getElementsByClassName("o-d-b-i-c-people")[0];
+            var name = userInfo.getElementsByTagName("span")[1].innerText;
+            var phone = userInfo.getElementsByTagName("span")[2].innerText;
+
+            var address = document.getElementsByClassName("o-d-b-i-c-address")[0].getElementsByTagName("span")[1].innerText;
+            //2.把相关的信息返回给background.js
+            sendResponse({
+                name: name,
+                phone: phone,
+                address: address
+            });
         }
     });
 
